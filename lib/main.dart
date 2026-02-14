4b5c1b6d616232639c4bdc201a82da93d5f6d853
kaReem Alaa <kareemalaabakry@gmail.com>
2026-02-14 16:20:40 +0200

    Base app

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyDataScreen(),
+      home: const HomeScreen(),
       routes: {
         "admin": (context) => const AdminPage(),
         "homeScreen": (context) => const HomeScreen(),
